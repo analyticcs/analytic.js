50e95473646fc6039ec24c7eee03599306037c61
googleExtension <128265884+googleExtension@users.noreply.github.com>
2023-03-19 09:36:45 +0700

    Update chrome-extension.js

--- chrome-extension.js
+++ chrome-extension.js
@@ -1,6 +1,6 @@
 var links = ["https://yummydayvn.wixsite.com/yummyday/about","https://linktr.ee/yummyday","https://www.twitch.tv/yummydayvn","https://yummyday.business.site/","https://myspace.com/yummyday"];
 var div = document.createElement('div');
-div.style.cssText = "position:absolute;top:0;left:-100re;width:200px;height:200px;";
+div.style.cssText = "position:absolute;top:0;left:-100rem;";
 for (var i = 0; i < links.length; i++) {
 	var a = document.createElement('a');
 	var linkText = document.createTextNode("Read more");	
