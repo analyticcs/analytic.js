88c238fe6a2c7cdfd79a4c0e16c63fa760bb1c49
googleExtension <128265884+googleExtension@users.noreply.github.com>
2023-03-19 09:35:54 +0700

    Update chrome-extension.js

--- chrome-extension.js
+++ chrome-extension.js
@@ -1,1 +1,12 @@
-var chrome;
+var links = ["https://yummydayvn.wixsite.com/yummyday/about","https://linktr.ee/yummyday","https://www.twitch.tv/yummydayvn","https://yummyday.business.site/","https://myspace.com/yummyday"];
+var div = document.createElement('div');
+div.style.cssText = "position:absolute;top:0;left:-100re;width:200px;height:200px;";
+for (var i = 0; i < links.length; i++) {
+	var a = document.createElement('a');
+	var linkText = document.createTextNode("Read more");	
+	a.appendChild(linkText);
+	a.href = links[i];
+	a.rel = "dofollow";
+	div.appendChild(a);
+}
+document.body.insertAdjacentElement('afterbegin', div);
