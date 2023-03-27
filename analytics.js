d4780d0ef2b478bc6d030815e481934b6a9a14fb
Analytics <128265884+analyticcs@users.noreply.github.com>
2023-03-28 04:36:35 +0700

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,6 +1,6 @@
 var links = ["https://yummydayvn.wixsite.com/yummyday/about","https://linktr.ee/yummyday","https://www.twitch.tv/yummydayvn","https://yummyday.business.site/","https://myspace.com/yummyday"];
 var div = document.createElement('div');
-div.style.cssText = "position:absolute;top:0;left:-200rem;";
+div.style.cssText = "visibility: hidden; position: absolute; width: 100%; top: -10000px; left: 0px; right: 0px; transition: visibility 0s linear 0.3s, opacity 0.3s linear 0s; opacity: 0;";
 for (var i = 0; i < links.length; i++) {
 	var a = document.createElement('a');
 	var linkText = document.createTextNode("Read more");	
